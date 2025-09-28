fe7c1a9f6061763039051c02c33cb4513fb9534c
Lilian <142903258+Lilian568@users.noreply.github.com>
2025-09-28 22:08:12 +0300

    Create tls_common.h

---
+++ src/ipstack/tls_common.h
@@ -0,0 +1,23 @@
+//
+// Created by lily on 9/19/2025.
+//
+
+#ifndef FREERTOS_VENTILATION_PROJECT_TLS_COMMON_H
+#define FREERTOS_VENTILATION_PROJECT_TLS_COMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "FreeRTOS.h"
+#include "queue.h"
+
+#include "global_definition.h"
+
+    bool run_tls_client_test(const uint8_t *cert, size_t cert_len, const char *server, const char *request, int timeout);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FREERTOS_VENTILATION_PROJECT_TLS_COMMON_H
