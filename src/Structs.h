84a42b010f106b40e32933578096e80e7de226b6
An Qi <aurelia@Ans-MacBook-Air.local>
2025-10-02 21:18:23 +0300

    minor changes

--- src/Structs.h
+++ src/Structs.h
@@ -1,8 +1,27 @@
-//
-// Created by An Qi on 1.10.2025.
-//
-
 #ifndef STRUCTS_H
 #define STRUCTS_H
+//all the structs that are needed in this project.
+
+//monitored data gathered from the sensors
+struct Monitored_data{
+    uint16_t co2_val;
+    double temperature;
+    double humidity;
+    double pressure;
+};
+
+//define the two types of messages to be sent to different queues
+enum MessageType{
+    MONITORED_DATA,
+    CO2_SET_DATA
+};
+
+//combine message type and data
+struct Message{
+    MessageType type;
+
+    Monitored_data data;
+    uint co2_set;
+};
 
 #endif //STRUCTS_H
